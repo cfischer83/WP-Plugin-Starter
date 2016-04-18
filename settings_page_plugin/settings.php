505d89bc251ec8a786379d7aac0f4fe6d08e222a
Cory Fischer <cfischer83@aol.com>
2016-04-18 14:43:55 -0700

    Settings Page Plugin

---
+++ settings_page_plugin/settings.php
@@ -0,0 +1,31 @@
+<?php
+/*
+	Plugin Name: PK Settings Page Plugin
+	Description: Setting up configurable fields for our plugin.
+	Author: Cory Fischer
+	Version: 1.0.0
+*/
+class Settings_Page_Plugin {
+	public function __construct() {
+		// Hook into the admin menu
+		add_action( 'admin_menu', array( $this, 'create_plugin_settings_page' ) );
+	}
+
+	public function create_plugin_settings_page() {
+		// Add the menu item and page
+		$page_title = 'My Awesome Settings Page';
+		$menu_title = 'Awesome Plugin';
+		$capability = 'manage_options';
+		$slug = 'test_fields';
+		$callback = array( $this, 'plugin_settings_page_content' );
+		$icon = 'dashicons-admin-plugins';
+		$position = 2;
+	
+		add_menu_page( $page_title, $menu_title, $capability, $slug, $callback, $icon, $position );
+	}
+
+	public function plugin_settings_page_content() {
+		echo 'Hello World!';
+	}
+}
+new Settings_Page_Plugin();
